b4727f990d8af2eb062ba2adf3467d4c0e3f0d11
keith <keith.lindsey216@gmail.com>
2022-04-08 12:05:38 -0400

    "first commit of code"

--- js.js
+++ js.js
@@ -2,9 +2,7 @@
 
 const boxes =document.querySelectorAll(".box");
 console.log(boxes)
-//const board = document.getElementById("gameBoard")
-//console.dir(board)
-const resetButton = document.getElementById('resetButton')
+const resetButtonContainer = document.getElementsByClassName('reset')
 console.dir(resetButton)
 const message = document.getElementById('gameText')
 console.dir(message)
@@ -13,10 +11,6 @@ const player_X = 'x'
 let currentPlayer = player_O
 let space=[null,null,null, null,null,null,null,null,null]
 
-let clicks = 0
-
-
-
 //Style____________________________________________________________________________
 
 function borderBottom(){
@@ -24,63 +18,55 @@ function borderBottom(){
     document.getElementById("1").style.borderBottom = "thick solid black";
     document.getElementById("2").style.borderBottom = "thick solid black";
 }
-
 function borderRight(){
     document.getElementById("0").style.borderRight ="thick solid black";
     document.getElementById("3").style.borderRight ="thick solid black";
     document.getElementById("6").style.borderRight ="thick solid black";
 }
-
 function borderLeft(){
     document.getElementById("2").style.borderLeft="thick solid black";
     document.getElementById("5").style.borderLeft="thick solid black";
     document.getElementById("8").style.borderLeft="thick solid black";
 }
-
 function borderTop(){
     document.getElementById("6").style.borderTop="thick solid black";
     document.getElementById("7").style.borderTop="thick solid black";
     document.getElementById("8").style.borderTop="thick solid black";
     
 }
-
-
-
 borderBottom()
 borderLeft()
 borderTop()
 borderRight()
-
-
 for (const box of boxes) {
     box.addEventListener('click',clickedBox)
 }
-
 resetButton.addEventListener('click', reset)
 console.log("clicked");
-//style______________________________________________________________________________
+//style_____________________________________________________________________________
 
-
-
-
+console.log(space)
 function clickedBox(e){
     
-    console.log(space);
     let id =e.target.id;
-    console.log(e);
+  
     
     if (!space[id]){
         space[id] = currentPlayer;
         e.target.innerText= currentPlayer
-    }
-    if(winning()){
-        message.innertext = `${currentPlayer} wins!!`;
-        reset();
-        return;
-        
-    } draw()
-    currentPlayer = currentPlayer === player_O ? player_X : player_O;
-    // if (currentPlayer)
+        if(winning()){
+            message.innerText = `${currentPlayer} wins!!`;
+            debugger
+            space.fill(1)
+            
+            return;
+            
+        } 
+        catGame()
+        currentPlayer = currentPlayer === player_O ? player_X : player_O
+
+    }    
+    
 }
 function winning(){
     if (space[0] === currentPlayer){
@@ -124,45 +110,23 @@ function winning(){
         }
     }
 };
-
-function draw(){
+function catGame(){
 
     if (!space.includes(null)){
         message.innerText = `cat game`;
     }
-    //for (let i = 0; clicks <9 ; i++)
-   
-//     if(space !== null){
-        
-//         clicks ++;
-//         console.log(clicks);
-//     }
-//     if (clicks === 9){
-//         message.innerText = `cat game`;
-//         //reset()
-//     }
-// }
+    
 }
      
     
 function reset(){
-
-    space = new Array (9*null)
-
+    boxes.forEach((el, i) => {
+        el.innerText = ""
+        space[i] = null
+    })
+    currentPlayer = player_O
+    console.log('player: ', currentPlayer)
+    console.log('dataSet: ', space)
+   message.innerText= "Tic Tac Toe"
 }
     
-//}
-
-
-//_________________________________________
-
-// function updateTextOnDomNode(text, node){
-//     node.innerText=text
-// }
-
-// function updateSpace(index, value){
-//     space[index]=value
-// }
-
-// updateSpace(3, "s")
-// console.log(space);
